52e94e48d99bdef37ba449ae0a04b556c6830ff3
mifodra <mifodra@gmail.com>
2023-08-14 09:10:35 -0300

    Adicionando anotações da pratica de variáveis

---
+++ DevAprender/primeiro_projeto/aula1.py
@@ -0,0 +1,17 @@
+### Variáveis ###
+# Números
+velocidade_internet = 10
+print(velocidade_internet)
+nota_filme = 8.5 #float
+
+# Valores booleanos (True/False)
+esta_aberto = True
+
+# Strings
+nome_do_curso = 'Lógica de Programação'
+
+# Exemplo 01: Valor por hora
+salario_mensal = input('Qual é o seu salário mensal?')
+horas_trabalhadas_por_mes = input('Quantas horas trabalha por mês?')
+valor_hora = int(salario_mensal) / int(horas_trabalhadas_por_mes)
+print(valor_hora)
